3245eb82102388871b7b3e73558ef1d82577ef23
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-06 14:23:33 -0600

    added player model

--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,12 @@
+import { Player } from './models/Player.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-
+  players = [
+    new Player('Jake')
+  ]
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
