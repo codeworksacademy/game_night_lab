fa79569960d8cef8b541f0bb4220bf5ab44590d2
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-06 14:24:27 -0600

    added controller

--- app/App.js
+++ app/App.js
@@ -1,11 +1,11 @@
+import { PlayersController } from './controllers/PlayersController.js';
 import { router } from './router-config.js';
-import { HomeController } from './controllers/HomeController.js';
 const USE_ROUTER = false
 
 class App {
 
-  HomeController = new HomeController()
 
+  PlayersController = new PlayersController()
   constructor() {
     if (USE_ROUTER) {
       this.router = router
